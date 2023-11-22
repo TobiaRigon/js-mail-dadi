a180b838d5eac58bf9a812b3836fc60c5cce2085
TobiaRigon <rigontobia@gmail.com>
2023-11-22 16:35:35 +0100

    ✨ Feat: create random numbers,  compare & Console

--- Dadi/js/script.js
+++ Dadi/js/script.js
@@ -1,17 +1,19 @@
 
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. 
 // numero random giocatore
-let PlayerNumber;
+let playerNumber = Math.floor(Math.random() * 6) + 1;
 // numero random pc
-let pcNumber;
+let pcNumber = Math.floor(Math.random() * 6) + 1;
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
 let result ;
-if(PlayerNumber>pcNumber){
-  risultato="win";
-}else if(PlayerNumber<pcNumber){
+if(playerNumber>pcNumber){
+  result="win";
+}else if(playerNumber<pcNumber){
   result="lose";
 }else{
   result="draw";
 }
 // Genera messaggio vittoria o sconfitta
+console.log("Numero del giocatore:",playerNumber);
+console.log("Numero del PC:", pcNumber);
 console.log(result);
