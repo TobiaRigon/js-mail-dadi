4fcc0f778ee11bf97dafe462164ee7621902a7f9
TobiaRigon <rigontobia@gmail.com>
2023-11-22 18:00:51 +0100

    🚩Feat: result color change

--- Dadi/js/script.js
+++ Dadi/js/script.js
@@ -20,8 +20,22 @@ playerButton.addEventListener("click", function() {
   console.log("Numero del PC:", pcNumber);
   console.log(result);
 
-  // Aggiorna l'HTML con i risultati
-  document.getElementById("player_result").innerHTML = `Your result is: ${playerNumber}`;
-  document.getElementById("pc_result").innerHTML = `PC result is: ${pcNumber}`;
-  document.getElementById("message").innerHTML = `You ${result}!`;
+ // Aggiorna l'HTML con i risultati e imposta il colore del testo
+const playerResultElement = document.getElementById("player_result");
+const pcResultElement = document.getElementById("pc_result");
+const messageElement = document.getElementById("message");
+
+playerResultElement.innerHTML = `Your result is: ${playerNumber}`;
+pcResultElement.innerHTML = `PC result is: ${pcNumber}`;
+messageElement.innerHTML = `You ${result}!`;
+
+// Imposta il colore del testo in base al risultato
+if (result === "won") {
+  messageElement.style.color = "green";
+} else if (result === "lost") {
+  messageElement.style.color = "red";
+} else {
+  // Se è un pareggio, puoi impostare il colore a un valore predefinito o cambiarlo a tuo piacimento
+  messageElement.style.color = "blue";
+}
 });
